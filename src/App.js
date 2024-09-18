78d1f8cf080ed6ca52b666ef1f5b8ba07785ec80
Guillaume Charpentier <guillaume.prodancer@gmail.com>
2024-09-18 16:26:58 +0200

    Add routes.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Error from "./pages/Error";
 import Housing from "./pages/Housing";
-import data from "./data/logements.json";
+import { home, about, housing } from "./routes";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/housing/:id" element={<Housing />} />
+        <Route path={home} element={<Home />} />
+        <Route path={about} element={<About />} />
+        <Route path={housing} element={<Housing />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
